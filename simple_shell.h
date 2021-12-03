ba3f9b9a6f6e5709d489b7fd0c1d62c585850f15
Secatricia <3560@holbertonschool.com>
2021-12-03 12:42:03 +0000

    Add new prototype simple_shell.h

--- simple_shell.h
+++ simple_shell.h
@@ -9,6 +9,12 @@
 #include <string.h>
 #include <sys/stat.h>
 
+typedef struct linked_path
+{
+	char *valeur;
+	struct linked_path *next;
+} path_t;
+
 int _strlen(char *s);
 char **strtow(char *str);
 int strwlen(char *s);
@@ -21,6 +27,10 @@ void *_realloc(void *ptr, unsigned int old_size, unsigned int new_size);
 void *_calloc(unsigned int nmemb, unsigned int size);
 int count_word(char *s);
 int _strcmp(char *s1, char *s2);
+char *_getenv(const char *name, char **env);
+path_t *linked_lp(path_t *head, char *value);
+path_t *create_linked_lp(char *the_path);
+void free_node(path_t *head);
 
 void _prompt(void);
 char *_getline(void);
